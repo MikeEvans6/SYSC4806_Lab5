62df24f9c1781bb1bcb8694780b4a36512272839
MichaelEvans6 <michael3vans66@gmail.com>
2021-02-12 20:23:31 -0500

    Lab 5 Upload

---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
